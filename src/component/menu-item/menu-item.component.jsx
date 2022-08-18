77ac9a2610d93262959230f5c92cc79390bb1d1f
rckstrpnda <rg200202@gmail.com>
2022-08-18 09:30:07 +0530

    css effect  added

--- src/component/menu-item/menu-item.component.jsx
+++ src/component/menu-item/menu-item.component.jsx
@@ -3,12 +3,14 @@ import "./menu-item.style.scss"
 
 const MenuItem = ({title,imageUrl,size}) =>(
             
-            <div style={{
+            <div  className={`${size} menu-item`}>
+                <div style={{
                 backgroundImage:`url(${imageUrl})`
-            }} className={`${size} menu-item`}>
+            }} className="background-image"
+                />
                 <div className="content">
-                    <h1 className="title">{title}</h1>
-                    <span className="subtitle">shop now</span>
+                    <h1 className="title">{title.toUpperCase()}</h1>
+                    <span className="subtitle">Shop Now</span>
                 </div>
             </div>
 );
